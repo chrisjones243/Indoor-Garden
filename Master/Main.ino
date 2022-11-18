28da056393afacafbb8ef00fa8d7bbde4d6966f0
chrisjones243 <38958680+chrisjones243@users.noreply.github.com>
2022-11-18 17:05:15 +0000

    Renamed and comment

---
+++ Master/Main.ino
@@ -0,0 +1,12 @@
+// This will contain the main code for the master device
+// and will be used to process the inputs and send the data to the slave device.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
