8a4fc87d8832efa61a9f49fc5fa33936fac2fef4
Jakotops <79404345+Jakotops@users.noreply.github.com>
2022-11-28 13:59:46 +0000

    Added comments to slave code

--- Slave/Main/Main.ino
+++ Slave/Main/Main.ino
@@ -7,13 +7,19 @@ int rs = 5, en = 7, d4 = 8, d5 = 9, d6 = 10, d7 = 11;
 
 // LCD object
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-int lightPin = 13;
+
+// Set the LED and the pump to the pins
+int LEDPin = 13;
 int pumpPin = 12;
+
+// Strores the current values displayed on the LCD 
+// This is to minimise the refreshes of the LCD display to prevent flickering
 int lightLCD = 101;
 int moistureLCD = 101;
 int LEDLCD = 0; // This is not a constant
 int pumpLCD = 0;
 
+// Custom character icons to represent light level and moisture level respectively 
 byte sun[] = {
   B10001,
   B11011,
@@ -35,16 +41,18 @@ byte water[] = {
   B11111,
   B01110
 };
-const char* moistureValue;
 
 void setup() {
-  //Sets the address of the arduino to 8
+  // Sets the address of the arduino to 8
   Wire.begin(8);
-  //Registers the event when the arduino recieves a transmission
+  // Registers the event when the arduino recieves a transmission
   Wire.onReceive(receiveEvent);
+  // Assigns the characters to a number
   lcd.createChar(0, sun);
   lcd.createChar(1, water);
-  pinMode(lightPin, OUTPUT);
+  // Sets the LED pin to output 
+  pinMode(LEDPin, OUTPUT);
+  // Initialises the display on the the LCD
   lcd.begin(16, 2);
   lcd.print("LED:OFF");
   lcd.setCursor(8, 0);
@@ -63,6 +71,7 @@ void setup() {
 void loop() {
 }
 
+// Turns the LEDs on/off and displays its status on the LCD
 void setLightState(bool state) {
   if (state == true){
     if (LEDLCD != 1){
@@ -78,6 +87,7 @@ void setLightState(bool state) {
   }
 }
 
+// Turns the water Solenoid on/off and displays its status on the LCD
 void setPumpState(bool state) {
   if (state == true){
     if (pumpLCD != 1){
@@ -93,16 +103,20 @@ void setPumpState(bool state) {
   }
 }
 
+//Turns the String into a constant char so it can be displayed on the LCD
 void displayValue(String value, int collumn, int row){
   const char* strValue = value.c_str();
   displayString(strValue, collumn, row);
 }
 
+// Clears the current display on the LCD and prints the new string onto the LCD
 void displayString(const char string[], int collumn, int row){
   lcd.setCursor(collumn - 2, row);
   lcd.print("   ");
   lcd.setCursor(collumn, row);
+  // Prints the string from right to left (e.g "  56" instead of "56 ")
   lcd.rightToLeft();
+  // Prints the last value first from right to left
   for (int i = strlen(string) - 1; i >= 0; i --){
     lcd.print(string[i]);
   }
@@ -130,6 +144,7 @@ void processCommand(String command) {
   }
   //Checks if the command is a light value command
   if (command.substring(0, 2) == "LV") {
+    // Only displays the recieved value if it is different to the currently displayed value on the LCD
     if (lightLCD != command.substring(2).toInt()){
       displayValue(command.substring(2), 5, 1);
       lightLCD = command.substring(2).toInt();
@@ -137,6 +152,7 @@ void processCommand(String command) {
   }
   //Checks if the command is a moisture value command
   if (command.substring(0, 2) == "MV") {
+    // Similar process to "LV" above
     if (moistureLCD != command.substring(2).toInt()){
       displayValue(command.substring(2), 12, 1);
       moistureLCD = command.substring(2).toInt();
@@ -144,6 +160,7 @@ void processCommand(String command) {
   }
 }
 
+// Makes sure the instrunction or message recieved is processed one at a time
 void receiveEvent(int bytes) {
   //This function is called when the arduino recieves a transmission
   String data = "";
